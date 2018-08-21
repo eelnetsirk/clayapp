1bb17185e59857945a0d0a737dabcbee2941f70a
shiny <kristenstuartlee@gmail.com>
2018-08-20 21:31:52 -0700

    integrate unsubscribe function

--- server.js
+++ server.js
@@ -51,7 +51,29 @@ app.post("/sub", function(req, res) {
   }
 
 })
+app.post("/unsub", function(req, res) {
+  console.log('got an unsub request')
+  console.log(req.body.data)
+  let list_of_subscribers = []
+  list_of_subscribers = fs.readFileSync(__dirname + '/list.txt').toString().split("\n")
 
+  if (list_of_subscribers.includes(req.body.data)) {
+    //we have found that the subscriber is in our list so unsubscribe and remove from list
+    for (var i=list_of_subscribers.length-1; i>=0; i--) {
+      if (list_of_subscribers[i] === req.body.data) {
+        list_of_subscribers.splice(i, 1)
+      }
+    }
+    console.log(list_of_subscribers)
+    //this line deletes the entire list on the disk!
+    fs.writeFile(__dirname + '/list.txt', '', function(){console.log('done')})
+    //takes the list and writes to file
+    for (var i = 0; i < list_of_subscribers.length; i++) {
+      fs.appendFile(__dirname + '/list.txt', list_of_subscribers[i] + "\n", function() {})
+
+    }
+  }
+})
 app.listen(8080,function(){
   console.log("Listening on port 8080!")
 })
